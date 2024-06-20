6e0be06eb5e6443412511647c140f46714e01232
Robert Wilson <robert.wilson23@nhs.net>
2024-06-20 14:51:35 +0100

    Organised existing section 0 and 1 pages into correct folders, and made many related routing fixes

--- app/routes.js
+++ app/routes.js
@@ -6,47 +6,47 @@ const router = express.Router();
 // Version 1
 
 // which-country-do-you-live-in.html
-router.post('/v1/which-country-do-you-live-in', function (request, response) {
+router.post('/v1/1-check-eligibility/which-country-do-you-live-in', function (request, response) {
     var country = request.session.data['country']
 
     if (country == "ni") {
-        response.redirect("/v1/northern-ireland")
+        response.redirect("northern-ireland")
     } else {
-        response.redirect("/v1/permanent-care-home")
+        response.redirect("permanent-care-home")
     }
 });
 
 // permanent-care-home.html
-router.post('/v1/permanent-care-home', function (request, response) {
+router.post('/v1/1-check-eligibility/permanent-care-home', function (request, response) {
     var careHome = request.session.data['care']
 
     if (careHome == "yes") {
-        response.redirect("/v1/council-help-pay")
+        response.redirect("council-help-pay")
     } else {
-        response.redirect("/v1/live-with-partner")
+        response.redirect("live-with-partner")
     }
 });
 
 
 // council-help-pay.html
-router.post('/v1/council-help-pay', function (request, response) {
+router.post('/v1/1-check-eligibility/council-help-pay', function (request, response) {
     var council = request.session.data['council']
 
     if (council == "yes") {
-        response.redirect("/v1/council-name")
+        response.redirect("council-name")
     } else {
         response.redirect("/v1/cannot-apply-online-yet")
     }
 });
 
 // live-with-partner.html
-router.post('/v1/live-with-partner', function (request, response) {
+router.post('/v1/1-check-eligibility/live-with-partner', function (request, response) {
     var livePartner = request.session.data['partner']
 
     if (livePartner == "yes") {
-        response.redirect("/v1/claimed-asylum-partner")
+        response.redirect("claimed-asylum-partner")
     } else {
-        response.redirect("/v1/claimed-asylum")
+        response.redirect("claimed-asylum")
     }
 });
 
@@ -55,9 +55,9 @@ router.post('/v1/who-is-applying', function (request, response) {
     var applying = request.session.data['applying']
 
     if (applying == "myself") {
-        response.redirect("/v1/what-is-your-name")
+        response.redirect("what-is-your-name")
     } else {
-        response.redirect("/v1/what-is-their-name")
+        response.redirect("what-is-their-name")
     }
 });
 
@@ -66,15 +66,15 @@ router.post('/v1/have-phone-number', function (request, response) {
     var phone = request.session.data['phone']
 
     if (phone == "yes") {
-        response.redirect("/v1/what-is-your-phone-number")
+        response.redirect("what-is-your-phone-number")
     } else {
-        response.redirect("/v1/have-email-address")
+        response.redirect("have-email-address")
     }
 });
 
 // what-is-your-phone-number.html
 router.post('/v1/what-is-your-phone-number', function (request, response) {
-    response.redirect("/v1/have-email-address")
+    response.redirect("have-email-address")
 });
 
 // have-email-address.html
@@ -82,15 +82,15 @@ router.post('/v1/have-email-address', function (request, response) {
     var email = request.session.data['email']
 
     if (email == "yes") {
-        response.redirect("/v1/what-is-your-email-address")
+        response.redirect("what-is-your-email-address")
     } else {
-        response.redirect("/v1/check-your-answers-contact-details")
+        response.redirect("check-your-answers-contact-details")
     }
 });
 
 // what-is-your-email-address.html
 router.post('/v1/what-is-your-email-address', function (request, response) {
-    response.redirect("/v1/check-your-answers-contact-details")
+    response.redirect("check-your-answers-contact-details")
 });
 
 // who-to-contact.html
@@ -98,9 +98,9 @@ router.post('/v1/who-to-contact', function (request, response) {
     var whoContact = request.session.data['whoContact']
 
     if (whoContact == "them") {
-        response.redirect("/v1/they-have-phone-number")
+        response.redirect("they-have-phone-number")
     } else {
-        response.redirect("/v1/who-to-contact-name")
+        response.redirect("who-to-contact-name")
     }
 });
 
@@ -109,15 +109,15 @@ router.post('/v1/they-have-phone-number', function (request, response) {
     var theyPhone = request.session.data['theyPhone']
 
     if (theyPhone == "yes") {
-        response.redirect("/v1/what-is-their-phone-number")
+        response.redirect("what-is-their-phone-number")
     } else {
-        response.redirect("/v1/they-have-email-address")
+        response.redirect("they-have-email-address")
     }
 });
 
 // what-is-their-phone-number.html
 router.post('/v1/what-is-their-phone-number', function (request, response) {
-    response.redirect("/v1/they-have-email-address")
+    response.redirect("they-have-email-address")
 });
 
 // they-have-email-address.html
@@ -125,15 +125,15 @@ router.post('/v1/they-have-email-address', function (request, response) {
     var theyEmail = request.session.data['theyEmail']
 
     if (theyEmail == "yes") {
-        response.redirect("/v1/what-is-their-email-address")
+        response.redirect("what-is-their-email-address")
     } else {
-        response.redirect("/v1/check-their-answers-contact-details")
+        response.redirect("check-their-answers-contact-details")
     }
 });
 
 // what-is-their-email-address.html
 router.post('/v1/what-is-their-email-address', function (request, response) {
-    response.redirect("/v1/check-their-answers-contact-details")
+    response.redirect("check-their-answers-contact-details")
 });
 
 // get-a-refund.html
@@ -141,15 +141,15 @@ router.post('/v1/get-a-refund', function (request, response) {
     var refund = request.session.data['refund']
 
     if (refund == "yes") {
-        response.redirect("/v1/refund-type")
+        response.redirect("refund-type")
     } else {
-        response.redirect("/v1/declaration")
+        response.redirect("declaration")
     }
 });
 
 // refund-type.html
 router.post('/v1/refund-type', function (request, response) {
-    response.redirect("/v1/declaration")
+    response.redirect("declaration")
 });
 
 // declaration.html
@@ -160,50 +160,50 @@ router.post('/v1/get-them-a-refund', function (request, response) {
     var themRefund = request.session.data['themRefund']
 
     if (themRefund == "yes") {
-        response.redirect("/v1/their-refund-type")
+        response.redirect("their-refund-type")
     } else {
-        response.redirect("/v1/their-declaration")
+        response.redirect("their-declaration")
     }
 });
 
 // their-refund-type.html
 router.post('/v1/their-refund-type', function (request, response) {
-    response.redirect("/v1/their-declaration")
+    response.redirect("their-declaration")
 });
 
 // their-declaration.html
 // The done page on test site seems to be broken / faulty but would go here
 
 // claimed-asylum-partner.html
-router.post('/v1/claimed-asylum-partner', function (request, response) {
+router.post('/v1/1-check-eligibility/claimed-asylum-partner', function (request, response) {
     var asylumPartner = request.session.data['asylumPartner']
 
     if (asylumPartner == "yes") {
         response.redirect("/v1/cannot-apply-online-yet")
     } else {
-        response.redirect("/v1/you-or-partner-education")
+        response.redirect("you-or-partner-education")
     }
 });
 
 // you-or-partner-education.html
-router.post('/v1/you-or-partner-education', function (request, response) {
+router.post('/v1/1-check-eligibility/you-or-partner-education', function (request, response) {
     var education = request.session.data['education']
 
     if (education == "yes") {
         response.redirect("/v1/cannot-apply-online-yet")
     } else {
-        response.redirect("/v1/partner-money-coming-in")
+        response.redirect("partner-money-coming-in")
     }
 });
 
 
 // partner-money-coming-in.html
-router.post('/v1/partner-money-coming-in', function (request, response) {
+router.post('/v1/1-check-eligibility/partner-money-coming-in', function (request, response) {
     var moneyComingIn = request.session.data['partnerMoneyComingIn']
 
     // If no checkboxes selected, stay on this page
     if (moneyComingIn == null) {
-        response.redirect("/v1/partner-money-coming-in")
+        response.redirect("partner-money-coming-in")
     }
 
     // Look at each checkbox: if any one of them has the value 'cant-apply', send them to the Cannot Apply page
@@ -214,17 +214,17 @@ router.post('/v1/partner-money-coming-in', function (request, response) {
     }
 
     // Otherwise, no 'cant-apply' checkboxes were checked, so proceed to next page
-    response.redirect("/v1/money-coming-in-2")
+    response.redirect("money-coming-in-2")
 });
 
 
 // money-coming-in-2.html
-router.post('/v1/money-coming-in-2', function (request, response) {
+router.post('/v1/1-check-eligibility/money-coming-in-2', function (request, response) {
     var moneyComingIn = request.session.data['moneyComingIn2']
 
     // If no checkboxes selected, stay on this page
     if (moneyComingIn == null) {
-        response.redirect("/v1/money-coming-in-2")
+        response.redirect("money-coming-in-2")
     }
 
     // Look at each checkbox: if any one of them has the value 'cant-apply', send them to the Cannot Apply page
@@ -235,63 +235,63 @@ router.post('/v1/money-coming-in-2', function (request, response) {
     }
 
     // Otherwise, no 'cant-apply' checkboxes were checked, so proceed to next page
-    response.redirect("/v1/more-than-6000-2")
+    response.redirect("more-than-6000-2")
 });
 
 
 // claimed-asylum.html
-router.post('/v1/claimed-asylum', function (request, response) {
+router.post('/v1/1-check-eligibility/claimed-asylum', function (request, response) {
     var asylum = request.session.data['asylum']
 
     if (asylum == "yes") {
-        response.redirect("/v1/asylum-application-decision")
+        response.redirect("asylum-application-decision")
     } else {
-        response.redirect("/v1/education-or-training")
+        response.redirect("education-or-training")
     }
 });
 
 // asylum-application-decision.html
-router.post('/v1/asylum-application-decision', function (request, response) {
+router.post('/v1/1-check-eligibility/asylum-application-decision', function (request, response) {
     var decision = request.session.data['decision']
 
     if (decision == "yes") {
-        response.redirect("/v1/education-or-training")
+        response.redirect("education-or-training")
     } else {
-        response.redirect("/v1/support-from-uk-visas-immigration")
+        response.redirect("support-from-uk-visas-immigration")
     }
 });
 
 // support-from-uk-visas-immigration.html
-router.post('/v1/support-from-uk-visas-immigration', function (request, response) {
+router.post('/v1/1-check-eligibility/support-from-uk-visas-immigration', function (request, response) {
     var support = request.session.data['support']
 
     if (support == "yes") {
-        response.redirect("/v1/full-exemption-asylum-decision")
+        response.redirect("full-exemption-asylum-decision")
     } else {
-        response.redirect("/v1/education-or-training")
+        response.redirect("education-or-training")
     }
 });
 
 // education-or-training.html
-router.post('/v1/education-or-training', function (request, response) {
+router.post('/v1/1-check-eligibility/education-or-training', function (request, response) {
     var training = request.session.data['training']
 
     if (training == "yes") {
-        response.redirect("/v1/financial-support")
+        response.redirect("financial-support")
     } else {
-        response.redirect("/v1/money-coming-in")
+        response.redirect("money-coming-in")
     }
 });
 
 
 // financial-support.html
-router.post('/v1/financial-support', function (request, response) {
+router.post('/v1/1-check-eligibility/financial-support', function (request, response) {
     var financialSupport = request.session.data['financialSupport']
     console.log(financialSupport);
 
     // If no checkboxes selected, stay on this page
     if (financialSupport == null) {
-        response.redirect("/v1/financial-support")
+        response.redirect("financial-support")
     }
 
     // Look at each checkbox: if any one of them has the value 'cant-apply', send them to the Cannot Apply page
@@ -302,18 +302,18 @@ router.post('/v1/financial-support', function (request, response) {
     }
 
     // Otherwise, no 'cant-apply' checkboxes were checked, so proceed to next page
-    response.redirect("/v1/money-coming-in")
+    response.redirect("money-coming-in")
 });
 
 
 // money-coming-in.html
-router.post('/v1/money-coming-in', function (request, response) {
+router.post('/v1/1-check-eligibility/money-coming-in', function (request, response) {
     var moneyComingIn = request.session.data['moneyComingIn']
     console.log(moneyComingIn);
 
     // If no checkboxes selected, stay on this page
     if (moneyComingIn == null) {
-        response.redirect("/v1/money-coming-in")
+        response.redirect("money-coming-in")
     }
 
     // Look at each checkbox: if any one of them has the value 'cant-apply', send them to the Cannot Apply page
@@ -324,29 +324,29 @@ router.post('/v1/money-coming-in', function (request, response) {
     }
 
     // Otherwise, no 'cant-apply' checkboxes were checked, so proceed to next page
-    response.redirect("/v1/more-than-6000")
+    response.redirect("more-than-6000")
 });
 
 
 // more-than-6000.html
-router.post('/v1/more-than-6000', function (request, response) {
+router.post('/v1/1-check-eligibility/more-than-6000', function (request, response) {
     var moreThan6000 = request.session.data['moreThan6000']
 
     if (moreThan6000 == "yes") {
         response.redirect("/v1/cannot-apply-online-yet")
     } else {
-        response.redirect("/v1/check-your-answers-check-eligibility")
+        response.redirect("check-your-answers-check-eligibility")
     }
 });
 
 // more-than-6000-2.html
-router.post('/v1/more-than-6000-2', function (request, response) {
+router.post('/v1/1-check-eligibility/more-than-6000-2', function (request, response) {
     var moreThan6000 = request.session.data['more-than-6000-2']
 
     if (moreThan6000 == "yes") {
         response.redirect("/v1/cannot-apply-online-yet")
     } else {
-        response.redirect("/v1/check-your-answers-check-eligibility-2")
+        response.redirect("check-your-answers-check-eligibility-2")
     }
 });
 
